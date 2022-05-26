66a53305473dcb55a091f33c3777ff2241ade744
shar699 <shar1061@sheridancollege.ca>
2022-05-26 09:33:58 -0400

    Week 3 example.

--- src/printingstudentdetail/Main.java
+++ src/printingstudentdetail/Main.java
@@ -27,6 +27,8 @@ public class Main {
         
         for(int i=0;i<list.length;i++){
             System.out.println(list[i].getName()+" "+list[i].getAge());
+            System.out.println("Learn Github operators");
+            
         }
         
     }
